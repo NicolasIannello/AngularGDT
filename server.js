80badba0c8657d91e63a31a9853c99bfc8c96971
NicolasIannello <nicolas.iannello99@gmail.com>
2022-01-05 10:11:53 -0300

    name

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/AGestor'));
+app.use(express.static(__dirname + '/dist/agestor'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/AGestor/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/agestor/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);

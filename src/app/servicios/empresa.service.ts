50c1ad2b1df21eb848685ea869ef9ae0aaaa0f91
NicolasIannello <nicolas.iannello99@gmail.com>
2022-03-31 14:26:13 -0300

    ruta

--- src/app/servicios/empresa.service.ts
+++ src/app/servicios/empresa.service.ts
@@ -11,8 +11,8 @@ export class EmpresaService {
   header:HttpHeaders;
 
   constructor(private http: HttpClient) {
-    this.servidor="http://localhost:8080";
-    //this.servidor="https://api-iannello.herokuapp.com";
+    //this.servidor="http://localhost:8080";
+    this.servidor="https://api-iannello.herokuapp.com";
     this.header=new HttpHeaders().set('Acces-Control-Allow-Origin','*');
   }
 
